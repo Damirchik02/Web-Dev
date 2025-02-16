e9c9e87b21c9da0b82b31d7e00ca419a1b469ab7
Damirchik02 <rdragnill@mail.ru>
2025-02-16 21:27:04 +0500

    Lab4

---
+++ Lab4/tutorial/1st tutorial/2.ts
@@ -0,0 +1,11 @@
+mport {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    Hello {{ city }}, {{ 1 + 1 }}
+  `,
+})
+export class AppComponent {
+  city = 'San Francisco';
+}
